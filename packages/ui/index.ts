c237265c2f91d49ea6fe7dba805f63230dae2276
nghiadg <nghiadt.dev@gmail.com>
2024-07-28 22:29:02 +0700

    feat(toggle): create toggle component for ui kit

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -2,3 +2,5 @@ export { default as Button } from './components/button/Button.svelte';
 export * from './components/button/Button.type';
 // Input
 export { default as Input } from './components/input/Input.svelte';
+// Toggle
+export { default as Toggle } from './components/toggle/Toggle.svelte';
