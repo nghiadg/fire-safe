8d5a6e50e028257a461288291090b2c8bf78ba1e
nghiadg <nghiadt.dev@gmail.com>
2024-07-28 22:09:03 +0700

    feat(radio): create radio component for ui kit

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -2,3 +2,5 @@ export { default as Button } from './components/button/Button.svelte';
 export * from './components/button/Button.type';
 // Input
 export { default as Input } from './components/input/Input.svelte';
+// Radio
+export { default as Radio } from './components/radio/Radio.svelte';
