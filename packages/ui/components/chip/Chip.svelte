372169f141d63c02910b88abd5828b58a8360b04
nghiadg <nghiadt.dev@gmail.com>
2024-07-29 23:14:31 +0700

    feat(chip): create chip component for ui kit

---
+++ packages/ui/components/chip/Chip.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { cx } from '../../helpers/tailwindcss.helper';
+  import { cva } from 'class-variance-authority';
+  import type { ChipKind } from './Chip.type';
+
+  const chipVariants = cva(['rounded text-text2 text-light-primary-text-color px-2 py-0.5'], {
+    variants: {
+      kind: {
+        primary: 'bg-light-primary-selected-color',
+        positive: 'bg-light-positive-color-selected',
+        negative: 'bg-light-negative-color-selected',
+        warning: 'bg-light-primary-warning-color-selected'
+      }
+    }
+  });
+
+  export let kind: ChipKind = 'primary';
+  export let disabled: boolean = false;
+  export let className = '';
+
+  $: chipClass = cx(chipVariants({ kind }), disabled && 'opacity-40', className);
+</script>
+
+<span class={chipClass}>
+  <slot />
+</span>
--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -4,3 +4,5 @@ export * from './components/button/Button.type';
 export { default as Input } from './components/input/Input.svelte';
 // Radio
 export { default as Radio } from './components/radio/Radio.svelte';
+// Chip
+export { default as Chip } from './components/chip/Chip.svelte';
